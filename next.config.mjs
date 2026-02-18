387ea820b203b97443a7fb8e61ac4cb8afe50ff5
minh <nqminhuit@gmail.com>
2026-02-18 16:33:23 +0700

    don't use trailingSlash

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,  // for static export only. Remove this if using server-side rendering
     domains: ['s3-api.prud.uk'],
