8537d22a4304da39ba23c704d44c110f0d4066a0
minh <nqminhuit@gmail.com>
2026-02-22 17:08:56 +0700

    remove: mockup data

--- components/CalendarSection.js
+++ components/CalendarSection.js
@@ -3,12 +3,6 @@
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-const events = [
-  { date: "7/7/2020", title: "Khai giảng lớp giáo lý" },
-  { date: "14/7/2020", title: "Tĩnh tâm giới trẻ" },
-  { date: "20/7/2020", title: "Chầu Thánh Thể" },
-];
-
 export default function CalendarSection({date, onChange}) {
   return (
     <div>
@@ -25,13 +19,6 @@ export default function CalendarSection({date, onChange}) {
           ? '!bg-green-500 !text-white !rounded-full'
           : '!rounded-full'}
         className="rounded-md shadow-lg p-1" />
-      <ul className="text-sm mt-4 text-gray-700 list-disc list-inside">
-        {events.map((e, idx) => (
-          <li key={idx}>
-            {e.date}: {e.title}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
